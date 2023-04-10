f0cf0b8692e0e33b96e05c1f94f0675e991c5762
asmaamoali <asmaaalimam832@gmail.com>
2023-04-10 07:13:27 -0700

    do

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,30 @@
 #include "main.h"
-unsigned int binary_to_uint(const char *b){
 
+/**
+ * binary_to_uint - A finction that converts binary number
+ * to an unsigned integer
+ * @b: A pointer to binary string
+ * Return: unsigned integer converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int base = 1, result = 0, len = 0;
 
-unsigned int result=0, ind=0, fase=1;
-
-if (b == NULL)
+	if (b == NULL)
 		return (0);
 
-	
+	while (b[len])
+		len++;
 
-	while (b!=NULL)
+	while (len)
 	{
-		
-		if (b[ind - 1] == '1')
-			result += fase;
-		fase *= 2;
-		ind--;
+		if (b[len - 1] != '0' && b[len - 1] != '1')
+			return (0);
+
+		if (b[len - 1] == '1')
+			result += base;
+		base *= 2;
+		len--;
 	}
 	return (result);
 }
